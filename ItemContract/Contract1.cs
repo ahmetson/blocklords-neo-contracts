69cfcb43e0414ba2dd16f9d32ba805d902585f04
Medet "Ahmetson" Atabayev <ahmetson@zoho.com>
2018-12-14 07:24:41 +0800

    Added DropItems method

--- ItemContract/Contract1.cs
+++ ItemContract/Contract1.cs
@@ -1,5 +1,6 @@
 ﻿using Neo.SmartContract.Framework;
 using Neo.SmartContract.Framework.Services.Neo;
+using Neo.SmartContract.Framework.Services.System;
 using System;
 using System.Numerics;
 
@@ -9,17 +10,20 @@ namespace LordsContract
     {
         private static readonly string MARKET_PREFIX = "\x01\x00";
         private static readonly string ITEM_PREFIX = "\x02\x00";
-        private static readonly string HERO_CREATION_PREFIX = "\x03\x00";
+        private static readonly string STRONGHOLD_PREFIX = "\x03\x00";
         private static readonly string ITEM_DROP_PREFIX = "\x04\x00";
         private static readonly string HERO_CREATION_KEY = "\x05";
         private static readonly string ITEM_DROP_KEY = "\x06";
         private static readonly string CITY_PREFIX = "\x07\x00";
         private static readonly string HERO_PREFIX = "\x08\x00";
+
         private static readonly byte HERO_CREATION_GIVEN = 0;
         private static readonly byte ITEM_DROP_GIVEN = 1;
 
         private static readonly BigInteger auctionFee = 5;  // In percents amount of GAS that buyers sends to Game Developers for Auction
         private static readonly BigInteger lordFee = 5;     // In percents default amount of GAS that buyers sends to City Lords for Auction 
+
+        private static readonly BigInteger DropInterval = 120; // Item will be dropped at each 120 blocks
 
         /**
          * 1 GAS === 100_000_000
@@ -67,13 +71,10 @@ namespace LordsContract
         [Serializable]
         public class DropData
         {
-            public BigInteger InsertedInitialItems;
-            public BigInteger GivenItems;
-            public BigInteger InsertedDroppableItems;
-            public BigInteger DroppedItems;
-            public BigInteger DropInterval;
-            public BigInteger LastDroppedBlock;
-            public BigInteger DroppedStrongholdId;
+            public BigInteger Block;
+            public BigInteger StrongholdId;
+            public BigInteger ItemId;
+            public BigInteger HeroId;
         }
 
         [Serializable]
@@ -94,6 +95,29 @@ namespace LordsContract
             public BigInteger SPEED;
             public BigInteger DEFENSE;
             public byte[] TX;
+        }
+
+        [Serializable]
+        public class Stronghold
+        {
+            public BigInteger ID;
+            public BigInteger Hero;
+        }
+
+        public static BigInteger GetRandomNumber()
+        {
+            //Transaction tx = (Transaction)ExecutionEngine.ScriptContainer;
+            //Header bl = Blockchain.GetHeader(Blockchain.GetHeight());
+            //byte[] h = bl.Hash.Concat(tx.Hash);
+            //var hash = Hash256(h);
+            //byte[] rand = hash.Range(0, size_in_bytes);
+            //return rand.AsBigInteger();
+
+            Header header = Blockchain.GetHeader(Blockchain.GetHeight());
+            ulong randomNumber = header.ConsensusData;
+            int percentage = (int)(randomNumber % 10);
+
+            return new BigInteger(percentage);
         }
 
         public static byte[] Main(string param, object[] args)
@@ -205,6 +229,17 @@ namespace LordsContract
 
                 //return new BigInteger(0).AsByteArray();
                 return AuctionEnd((BigInteger)args[0]);
+            }
+            else if (param.Equals("dropItems"))
+            {
+                // Check Witnesses
+                if (!Runtime.CheckWitness(GameOwner))
+                {
+                    Runtime.Log("Permission denied! Only game admin can add new items. Atleast for now!");
+                    return new BigInteger(0).AsByteArray();
+                }
+
+                return DropItems();
             }
 
             //Runtime.Notify("Incorrect Parameter");
@@ -413,7 +448,7 @@ namespace LordsContract
             return new BigInteger(1).AsByteArray();
         }
 
-        public static void UpdateDropData()
+        public static void UpdateItems()
         {
 
         }
@@ -502,14 +537,80 @@ namespace LordsContract
         //
         //------------------------------------------------------------------------------------
 
-        public static void GiveItems()
+        public static byte[] DropItems()
         {
+            // Check the Validness of the time
+            DropData dropData = (DropData)Neo.SmartContract.Framework.Helper.Deserialize(Storage.Get(Storage.CurrentContext, ITEM_DROP_KEY));
 
-        }
+            BigInteger currentBlock = Blockchain.GetHeight();
 
-        public static void DropItems()
-        {
+            if (dropData != null && currentBlock < dropData.Block + DropInterval)
+            {
+                Runtime.Notify("To early to drop Items! Last Block", dropData.Block, "Current Block", currentBlock);
+                return new BigInteger(0).AsByteArray();  
+            }
 
+            // Get Random Number
+            BigInteger stronghold = GetRandomNumber();
+            Runtime.Notify("Returned Random Stronghold", stronghold);
+
+            string key = STRONGHOLD_PREFIX + stronghold.AsByteArray();
+            byte[] inStronghold = Storage.Get(Storage.CurrentContext, key);
+
+            if (inStronghold.Length == 0)
+            {
+                Runtime.Notify("Stronghold is owned by NPC");
+                return new BigInteger(0).AsByteArray();
+            }
+
+            // Check existence of Drop Item
+            var nextDropItem = Storage.Get(Storage.CurrentContext, ITEM_DROP_KEY);
+            if (nextDropItem.Length == 0)
+            {
+                Runtime.Notify("Item to drop doesn't exists");
+                return new BigInteger(0).AsByteArray();
+            }
+
+            // If Drop Item exists get item id from it
+            string itemKey = ITEM_PREFIX + nextDropItem;
+            Item item = (Item)Neo.SmartContract.Framework.Helper.Deserialize(Storage.Get(Storage.CurrentContext, itemKey));
+
+            string heroKey = HERO_PREFIX + inStronghold;
+            Hero hero = (Hero)Neo.SmartContract.Framework.Helper.Deserialize(Storage.Get(Storage.CurrentContext, heroKey));
+
+
+           
+            // Change owner of Item.
+            item.OWNER = hero.OWNER;
+            byte[] itemBytes = Neo.SmartContract.Framework.Helper.Serialize(item);
+
+            // Change Drop Item.
+            Storage.Put(Storage.CurrentContext, itemKey, itemBytes);
+
+            // Delete Stronghold owner. (It means "kicking out from Stronghold")
+            Storage.Delete(Storage.CurrentContext, key);
+            
+            key = ITEM_DROP_PREFIX + nextDropItem;
+            BigInteger previousItem = new BigInteger(Storage.Get(Storage.CurrentContext, key)); // Previous Added item is linked as previous item
+
+            // Set the drop item
+            Storage.Delete(Storage.CurrentContext, key);
+            Storage.Put(Storage.CurrentContext, ITEM_DROP_KEY, previousItem); // Set Prvious Drop item as next droppable item
+
+
+            // Set Stronghold update time.
+            DropData dropped = new DropData();
+            dropped.Block = Blockchain.GetHeight();
+            dropped.HeroId = inStronghold.AsBigInteger();
+            dropped.ItemId = nextDropItem.AsBigInteger();
+            dropped.StrongholdId = stronghold;
+
+            byte[] bytes = Neo.SmartContract.Framework.Helper.Serialize(dropped);
+
+            Storage.Put(Storage.CurrentContext, ITEM_DROP_KEY, bytes);
+
+            Runtime.Notify("Item was Dropped successfully");
+            return new BigInteger(1).AsByteArray();
         }
 
         //------------------------------------------------------------------------------------
@@ -520,8 +621,9 @@ namespace LordsContract
 
         public static void UpdateItemStats(BigInteger[] ids, BigInteger[] newStats)
         {
-
+            Runtime.Notify("Init Item Stat Update");
         }
+
 
         //------------------------------------------------------------------------------------
         private static bool IsAuctionTransactionFeeIncluded ( BigInteger duration )
